4034e679d9e26b954bcdc38b64432d8d21424531
rifayetazamtalukder <rifayetewucse@gmail.com>
2019-12-18 18:00:21 +0600

    Combobox added for all input

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,76 @@ namespace CGPA_Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        public int s1Credit = 0;
+        /*public int s2Credit = 0;
+        public int s3Credit = 0;
+        public int s4Credit = 0;
+        public int s5Credit = 0;
+
+        public double s1Grade = 0.0D;
+        public double s2Grade = 0.0D;
+        public double s3Grade = 0.0D;
+        public double s4Grade = 0.0D;
+        public double s5Grade = 0.0D;*/
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void GpaButton_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            GpaValue.Text = "GPA: 4.00";
+        }
+
+        private void CgpaButton_Click(object sender, RoutedEventArgs e)
+        {
+            CgpaValue.Text = "CGPA: 4.00";
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Subject1Credit_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // var credit = GetCredit(object sender);
+
+            if(this.GpaValue == null)
+            {
+                return;
+            }
+
+            var combobox = (ComboBox)sender;
+
+            var value = (ComboBoxItem)combobox.SelectedValue;
+
+            var content = (string)value.Content;
+
+            try
+            {
+                s1Credit = int.Parse(content);
+            }
+            catch (Exception)
+            {
+                this.GpaValue.Text = content;
+                return;
+
+            }
+
+            this.GpaValue.Text = s1Credit.ToString();
+        }
+
+        //
+        public void showSelectedBox(object sender)
+        {
+            var combobox = this.Subject1Credit.SelectedValue;
+
+            var val = (string)combobox;
+
+            CgpaValue.Text = val;
         }
     }
 }
